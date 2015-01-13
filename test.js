98496c6ef9858638e1f7cae984ccbe42691d9989
Andy Richardson <rishson@gmail.com>
2015-01-13 15:36:21 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function func1(opts) {
   console.log(opts);
   console.log('hello');  
-  console.log('world')
-  console.log('goodbye')
-  console.log('world')
+  console.log('world');
+  console.log('goodbye');
+  console.log('world');
 }
