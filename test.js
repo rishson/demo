648720f52e9fb2dc9aa00a6894c95189b539bed1
Andy Richardson <rishson@gmail.com>
2015-03-06 11:54:44 +0000

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,14 @@ function func1(opts) {
   console.log('goodbye');
   console.log('world');
 }
+
+function func1(opts) {  //unused param on function declaration line
+  console.log('hello'); //good line 
+  console.log('world')  //issue line
+
+  while(true){
+    var x = new function(unusedParam){
+      console.log('inside a function')  //nested issue line
+    }
+  }
+}
